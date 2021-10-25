9d5bdc41fb4b3056ac9b3ff6376af5441c01db83
Mikoto <suomikoto@outlook.com>
2021-10-25 22:08:32 +0800

    add a new method

--- Problems/Depth First Search/90. Subset II.py
+++ Problems/Depth First Search/90. Subset II.py
@@ -21,7 +21,7 @@ class Solution:
                 # 第一次从 1 开始看，发现第二个相同 2 时就跳出
                 # 然后下次从 2 开始看，可以容忍第一个相同的 2，但在第二个相同的 2 退出
                 # 再下次从第二个 2 开始看，可以容忍第一个相同的 2
-                if i > idx and nums[i] == nums[i-1]:
+                if i > idx and nums[i] == nums[i - 1]:
                     continue
                 cur.append(nums[i])
                 dfs(cur, i + 1)
@@ -31,6 +31,37 @@ class Solution:
 
         return res
 
+    def subsetsWithDup_v2(self, nums: List[int]) -> List[List[int]]:
+
+        res = [[]]
+
+        nums.sort()
+        pre = None
+        for n in nums:
+            tmp = []
+            dup = dup + 1 if n == pre else 0
+            for r in res:
+                # 如果当前没有重复，那么直接给所有的都追加
+                if not dup:
+                    tmp.append(r + [n])
+                    continue
+                # 如果有重复（比如当前是第 k 个相同数字）
+                # 那么想要追加，需要满足下列要求：
+                # 要被追加的数组的倒数 (k-1) 个数字必须都是这个数
+                if len(r) < dup:
+                    continue
+                flag = True
+                for i in range(-1, -1 - dup, -1):
+                    if r[i] != n:
+                        flag = False
+                        break
+                if flag:
+                    tmp.append(r + [n])
+            res.extend(tmp)
+            pre = n
+
+        return res
+
 
 s = Solution()
-print(s.subsetsWithDup([1, 2, 2, 2]))
+print(s.subsetsWithDup_v2([1, 2, 2, 2]))
